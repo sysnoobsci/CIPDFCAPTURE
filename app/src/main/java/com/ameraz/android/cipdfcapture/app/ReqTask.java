0932071088a3b7c66a631da2044a2c3492caa964
sysnoobsci <sysnoobsci@gmail.com>
2014-06-24 16:43:11 -0500

    Initial commit for project

---
+++ app/src/main/java/com/ameraz/android/cipdfcapture/app/ReqTask.java
@@ -0,0 +1,134 @@
+package com.ameraz.android.cipdfcapture.app;
+
+/**
+ * Created by adrian.meraz on 6/24/2014.
+ */
+        import android.app.ProgressDialog;
+        import android.content.Context;
+        import android.os.AsyncTask;
+        import android.util.Log;
+
+        import org.apache.http.HttpEntity;
+        import org.apache.http.HttpResponse;
+        import org.apache.http.client.ClientProtocolException;
+        import org.apache.http.client.HttpClient;
+        import org.apache.http.client.methods.HttpPost;
+        import org.apache.http.entity.BufferedHttpEntity;
+        import org.apache.http.impl.client.DefaultHttpClient;
+
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+
+
+class ReqTask extends AsyncTask<String, Void, String> {
+
+    String result = "No result";
+
+    HttpClient httpclient = new DefaultHttpClient();
+    HttpPost httppost = new HttpPost("http://www.yoursite.com/");
+
+    private String query;
+    private String className;
+    private Context mContext;
+    private static int taskID = 0;
+
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public Context getActContext() {
+        return mContext;
+    }
+
+    public void setActContext(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public int getTaskID() {
+        return taskID;
+    }
+
+    public void setTaskID(int taskID) {
+        this.taskID = taskID;
+    }
+
+    public ReqTask(String query, String className, Context context){
+        setTaskID(this.taskID);//set unique ID for task
+        setQuery(query);
+        setClassName(className);
+        Log.d("Variable", "ReqTask" + getTaskID() + " context value: " + context);
+        setActContext(context);
+        taskID++;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        //dialog = ProgressDialog.show(getActivityContext(),"Logging in...","Please Wait",false);
+    }
+
+    @Override
+    protected String doInBackground(String... args) {
+        XmlParser xmlobj = new XmlParser();
+        StringBuilder total = new StringBuilder();
+        try {
+            HttpPost httptemp = new HttpPost(getQuery());//form http req string and assign to httppost
+            Log.d("Variable", getClassName() + ".java - query input result: " + getQuery());
+            httppost = httptemp;
+            // Execute HTTP Post Request
+
+            HttpResponse response = httpclient.execute(httppost);
+
+            HttpEntity ht = response.getEntity();
+
+            BufferedHttpEntity buf = new BufferedHttpEntity(ht);
+
+            InputStream is = buf.getContent();
+
+            BufferedReader r = new BufferedReader(new InputStreamReader(is));
+
+            String line;
+            while ((line = r.readLine()) != null) {
+                total.append(line);
+            }
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        Log.d("Variable", "Caller " + getClassName() + ".java - total.toString() result: " + total.toString());
+        if (!xmlobj.isXMLformat(total.toString())) {
+            Log.e("XMLFormatError", getClassName() + ".java - XML is malformed");
+        }
+        setResult(total.toString());
+        return total.toString();
+    }
+
+    protected void onPostExecute(String result) {
+        Log.d("Variable", "Task Executed from " + this.getClass().getName() + " + result: " + result);
+    }
+}//end of ReqTask
+
---
+++ app/src/main/java/com/ameraz/android/cipdfcapture/app/XmlParser.java
@@ -0,0 +1,104 @@
+package com.ameraz.android.cipdfcapture.app;
+
+/**
+ * Created by adrian.meraz on 6/24/2014.
+ */
+import android.util.Log;
+import android.util.Xml;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+/**
+ * Created by adrian.meraz on 5/16/2014.
+ */
+public class XmlParser {
+    private Boolean is_xml = false;
+    private String xmlstring = "";
+
+    private final static String EMPTY_STRING = "";
+
+    ArrayList<String> textTag = new ArrayList<String>();
+
+    public String getXmlstring() {
+        return xmlstring;
+    }
+
+    public void setXmlstring(String xmlstring) {
+        this.xmlstring = xmlstring;
+    }
+
+    static StringBuilder total = new StringBuilder();
+
+    public Boolean getIs_xml() {
+        return is_xml;
+    }
+
+    public void setIs_xml(Boolean is_xml) {
+        this.is_xml = is_xml;
+    }
+
+    public ArrayList<String> getTextTag() {
+        return textTag;
+    }
+
+    public void setTextTag(ArrayList<String> textTag) {
+        this.textTag = textTag;
+    }
+
+    public String parseXMLfunc(String xmlstring)
+            throws XmlPullParserException, IOException{
+
+        ArrayList<String> listOfTextTags = new ArrayList<String>();//a list contain all the text inside XML tags
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+        XmlPullParser xpp = factory.newPullParser();
+
+        xpp.setInput( new StringReader ( xmlstring ) );
+        int eventType = xpp.getEventType();
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            if(eventType == XmlPullParser.START_DOCUMENT) {
+                //total.append("Start document\n");
+            } else if(eventType == XmlPullParser.START_TAG) {
+
+                //total.append("Start tag "+xpp.getName()+"\n");
+            } else if(eventType == XmlPullParser.END_TAG) {
+                //total.append("End tag "+xpp.getName()+"\n");
+            } else if(eventType == XmlPullParser.TEXT) {
+                listOfTextTags.add(xpp.getText());
+                total.append(xpp.getText() + ",");
+                //total.append("Text "+xpp.getText()+"\n");
+            }
+            eventType = xpp.next();
+        }
+        //total.append("End document\n");
+        setTextTag(listOfTextTags);
+        setXmlstring(total.toString());
+        Log.d("XML", "Contents of XML Response: " + getXmlstring());
+        return total.toString();//return parsed contents of XML
+    }
+
+    public void clearXMLString(){
+        setXmlstring(EMPTY_STRING);
+    }
+
+    public String findTagText(String tag){
+        String tagText = "";
+        if(tag.equals(EMPTY_STRING)){//if nothing is being searched for, return all the xml results
+            return getXmlstring();
+        }
+        return tagText;
+    }
+
+    protected Boolean isXMLformat(String xmlstring){
+        String str2 = "<?xml version=";
+        setIs_xml(xmlstring.toLowerCase().contains(str2.toLowerCase()));
+        return getIs_xml();
+    }
+
+}
