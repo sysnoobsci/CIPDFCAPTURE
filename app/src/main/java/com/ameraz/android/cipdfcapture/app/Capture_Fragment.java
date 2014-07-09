cbdb335ec75c45298bb22f0b69cbe4b52ff69589
John Williams <john.williams@systemware.com>
2014-07-09 15:44:47 -0500

    small changes, trying to get it to work on adrians phone

--- app/src/main/java/com/ameraz/android/cipdfcapture/app/Capture_Fragment.java
+++ app/src/main/java/com/ameraz/android/cipdfcapture/app/Capture_Fragment.java
@@ -4,7 +4,6 @@ import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -31,9 +30,11 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 
 /**
- * Created by The Bat Cave on 6/2/2014.
+ * Created by John Williams on 6/2/2014.
  */
 public class Capture_Fragment extends Fragment {
+    static byte[] bArray;
+    static com.itextpdf.text.Image image;
     private ImageView imageView;
     private ImageButton takePic;
     private ImageButton searchGallery;
@@ -43,12 +44,8 @@ public class Capture_Fragment extends Fragment {
     private Uri imageUri;
     private String incImage;
     private File newImage;
-
     private Bitmap myImage;
     private Bitmap bm;
-
-    static byte[] bArray;
-    static com.itextpdf.text.Image image;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -75,104 +72,18 @@ public class Capture_Fragment extends Fragment {
                     ToastMessageTask tmtask = new ToastMessageTask(getActivity(), "Nothing to save.");
                     tmtask.execute();
                 } else {
-                    try {
+/*                    try {
                         myImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
                         bm = myImage.createScaledBitmap(myImage, 1080, 1920, true);
+                        //destroyBitmap();
                         imageView.setImageBitmap(bm);
                     } catch (Exception e) {
                         Log.d("Error: ", e.toString());
-                    }
+                    }*/
                     new saveImageAsPDF().execute();
                 }
             }
         });
-    }
-
-
-    private class saveImageAsPDF extends AsyncTask {
-        ProgressDialog dialog;
-
-
-        @Override
-        protected void onPreExecute(){
-            dialog = ProgressDialog.show(getActivity(),"", "Converting your shit", false);
-        }
-
-        @Override
-        protected Object doInBackground(Object[] params) {
-            makeSystemwareDir();
-
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
-            bArray = stream.toByteArray();
-
-            try
-            {
-                Document document = new Document();
-                String file = "/storage/sdcard0/Systemware/sys"+ System.currentTimeMillis() + ".pdf";
-                PdfWriter.getInstance(document, new FileOutputStream(file));
-                document.setPageSize(PageSize.A5);
-                document.open();
-
-                addImage(document);
-                document.close();
-            }
-
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-
-
-            return null;
-        }
-        private void addImage(Document document){
-
-            try{
-                image = com.itextpdf.text.Image.getInstance(bArray);
-            }
-            catch (BadElementException e){
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            catch (MalformedURLException e){
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            catch (IOException e){
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            int indentation = 0;
-            float scaler = ((document.getPageSize().getWidth() - document.leftMargin()
-                    - document.rightMargin() - indentation) / image.getWidth()) * 100;
-
-            image.scalePercent(scaler);
-            try{
-                document.add(image);
-            } catch (DocumentException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-
-        private void makeSystemwareDir() {
-            File directory = new File("/storage/sdcard0/Systemware/");
-            try {
-                directory.mkdirs();
-            }catch(Exception e){
-                Log.d("what's mah problem: ", e.toString());
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Object result){
-            dialog.dismiss();
-            imageView.setImageResource(R.drawable.sw_background);
-            imageUri = null;
-            ToastMessageTask tmtask = new ToastMessageTask(getActivity(), "Your image has been saved.");
-            tmtask.execute();
-        }
     }
 
     private void navFileSystemListener() {
@@ -189,7 +100,7 @@ public class Capture_Fragment extends Fragment {
         searchGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(i, 1);
             }
         });
@@ -201,7 +112,7 @@ public class Capture_Fragment extends Fragment {
             public void onClick(View v) {
 
                 String storageState = Environment.getExternalStorageState();
-                if(storageState.equals(Environment.MEDIA_MOUNTED)) {
+                if (storageState.equals(Environment.MEDIA_MOUNTED)) {
                     incImage = "/storage/sdcard0/Systemware/sys_original_image" + System.currentTimeMillis() + ".jpg";
                     newImage = new File(incImage);
                     try {
@@ -226,50 +137,149 @@ public class Capture_Fragment extends Fragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch(requestCode){
-            case 0:
-                if(resultCode == getActivity().RESULT_OK)
+        if (resultCode == getActivity().RESULT_OK) {
+            switch (requestCode) {
+                case 0:
+
                     try {
                         myImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
                         bm = myImage.createScaledBitmap(myImage, 1080, 1920, true);
+                        destroyBitmap();
                         imageView.setImageBitmap(bm);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                break;
-            case 1:
+                    break;
+                case 1:
 
-                if(resultCode == getActivity().RESULT_OK)
-                imageUri = data.getData();
-                try {
-                    myImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
-                    bm = myImage.createScaledBitmap(myImage, 1080, 1920, true);
-                    imageView.setImageBitmap(bm);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                    imageUri = data.getData();
+                    try {
+                        myImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
+                        bm = myImage.createScaledBitmap(myImage, 1080, 1920, true);
+                        destroyBitmap();
+                        imageView.setImageBitmap(bm);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
-                break;
-            case 2:
-                String loc = "file://" + data.getStringExtra("GetPath") + "/" + data.getStringExtra("GetFileName");
-                imageUri = Uri.parse(loc);
-                try {
-                    myImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
-                    bm = myImage.createScaledBitmap(myImage, 1080, 1920, true);
-                    imageView.setImageBitmap(bm);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                break;
+                    break;
+                case 2:
+
+                    String loc = "file://" + data.getStringExtra("GetPath") + "/" + data.getStringExtra("GetFileName");
+                    imageUri = Uri.parse(loc);
+                    try {
+                        //myImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
+                        bm = myImage.createScaledBitmap(MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri), 1080, 1920, true);
+                        destroyBitmap();
+                        imageView.setImageBitmap(bm);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
-//stuff was changed
+
+    //stuff was changed
     private void initializeViews(View rootView) {
-        imageView = (ImageView)rootView.findViewById(R.id.imageView);
-        takePic = (ImageButton)rootView.findViewById(R.id.capture_new_pic);
-        searchGallery = (ImageButton)rootView.findViewById(R.id.capture_gallery);
-        navFileSystem = (ImageButton)rootView.findViewById(R.id.capture_nav);
-        savePDF = (ImageButton)rootView.findViewById(R.id.capture_save);
-        sharePDF = (ImageButton)rootView.findViewById(R.id.capture_share);
+        imageView = (ImageView) rootView.findViewById(R.id.imageView);
+        takePic = (ImageButton) rootView.findViewById(R.id.capture_new_pic);
+        searchGallery = (ImageButton) rootView.findViewById(R.id.capture_gallery);
+        navFileSystem = (ImageButton) rootView.findViewById(R.id.capture_nav);
+        savePDF = (ImageButton) rootView.findViewById(R.id.capture_save);
+        sharePDF = (ImageButton) rootView.findViewById(R.id.capture_share);
+    }
+
+    private class saveImageAsPDF extends AsyncTask {
+        ProgressDialog dialog;
+
+
+        @Override
+        protected void onPreExecute() {
+            dialog = ProgressDialog.show(getActivity(), "", "Converting and saving as PDF", false);
+        }
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            makeSystemwareDir();
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            bArray = stream.toByteArray();
+
+            try {
+                Document document = new Document();
+                File f = new File(Environment.getExternalStorageDirectory() + "/Systemware");
+                if(!f.isDirectory()){
+                    f.mkdir();
+                }
+                String fileName = Environment.getExternalStorageDirectory() + "/Systemware/sys" + System.currentTimeMillis() + ".pdf";
+                File file = new File(fileName);
+
+                PdfWriter.getInstance(document, new FileOutputStream(file));
+                document.setPageSize(PageSize.A5);
+                document.open();
+
+                addImage(document);
+                document.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        private void addImage(Document document) {
+
+            try {
+                image = com.itextpdf.text.Image.getInstance(bArray);
+            } catch (BadElementException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            int indentation = 0;
+            float scaler = ((document.getPageSize().getWidth() - document.leftMargin()
+                    - document.rightMargin() - indentation) / image.getWidth()) * 100;
+            image.scalePercent(scaler);
+            try {
+                document.add(image);
+            } catch (DocumentException e) {
+                Log.d("What's going on: ", e.toString());
+                e.printStackTrace();
+            }
+        }
+
+        private void makeSystemwareDir() {
+            File directory = new File("/storage/sdcard0/Systemware/");
+            try {
+                directory.mkdirs();
+            } catch (Exception e) {
+                Log.d("what's mah problem: ", e.toString());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Object result) {
+            dialog.dismiss();
+            imageView.setImageResource(R.drawable.sw_background);
+            imageUri = null;
+            ToastMessageTask tmtask = new ToastMessageTask(getActivity(), "Your image has been saved.");
+            tmtask.execute();
+        }
+    }
+
+    public void destroyBitmap(){
+        myImage.recycle();
+        myImage = null;
     }
 }
