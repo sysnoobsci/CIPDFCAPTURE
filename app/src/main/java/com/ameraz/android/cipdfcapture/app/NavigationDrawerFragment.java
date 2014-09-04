75ef5b29976c42e4362aa6d6e7d88eb1b0491ca6
sysnoobsci <sysnoobsci@gmail.com>
2014-09-04 16:54:29 -0500

    Refactored code and remove extraneous classes

--- app/src/main/java/com/ameraz/android/cipdfcapture/app/NavigationDrawerFragment.java
+++ app/src/main/java/com/ameraz/android/cipdfcapture/app/NavigationDrawerFragment.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * Fragment used for managing interactions for and presentation of a navigation drawer.
+ * Fragment used for managing interactions for and presentation of a navigation drawer.s
  * See the <a href="https://developer.android.com/design/patterns/navigation-drawer.html#Interaction">
  * design guidelines</a> for a complete explanation of the behaviors implemented here.
  */
